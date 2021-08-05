fa599f97503140f21e95afb3c702b3439333e018
chgopikrishnamca <chgopikrishnamca@gmail.com>
2021-08-05 23:08:34 +0530

    Blogs are now added to the Site.

--- Blog/views.py
+++ Blog/views.py
@@ -1,3 +1,6 @@
+from Blog.models import Blog
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    blogs = Blog.objects.order_by('-date')[:5] # [:5] tells to pick last 5 recent blogs
+    return render(request, 'Blog/home.html', {'blogs': blogs})
