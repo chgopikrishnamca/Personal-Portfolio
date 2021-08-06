a5e21731360986c2893a2ba61ddc8506dbf39c08
chgopikrishnamca <chgopikrishnamca@gmail.com>
2021-08-07 00:23:21 +0530

    Done with the site. Need to add some styling

--- Blog/models.py
+++ Blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     title = models.CharField(max_length=100)
     date = models.DateTimeField()
     description = models.TextField(max_length=1000)
+
+    def __str__(self):
+        return self.title
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -6,3 +6,4 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, "PortFolio/home.html", {'projects': projects})
+
