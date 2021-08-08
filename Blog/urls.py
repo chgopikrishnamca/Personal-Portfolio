719c616dae05da341c4ebd14071914b761f0e8ce
chgopikrishnamca <chgopikrishnamca@gmail.com>
2021-08-08 20:08:03 +0530

    Project Completed as intial stage. This can be implemented in future as per needs.

--- Blog/urls.py
+++ Blog/urls.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 app_name="blog_app"
 
 urlpatterns = [
     path('', views.blog_home, name='blog_home'),
-    path('home/', views.blog_home, name='blog_home'),
-    path('<int:blog_id>/', views.blog_details, name='blog_details')
+    path('<int:blog_id>/', views.blog_details, name='blog_details'),
 ]   
--- Portfolio/views.py
+++ Portfolio/views.py
@@ -2,7 +2,6 @@ from django.http.response import HttpResponse
 from django.shortcuts import render
 from .models import Project
 
-# Create your views here.
 def home(request):
     projects = Project.objects.all()
     return render(request, "PortFolio/home.html", {'projects': projects})
